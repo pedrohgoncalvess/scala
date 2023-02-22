5976ab762c1250c57463171f4215c88a23aed678
Pedro H <pedro_gonsalves@hotmail.com>
2023-02-22 12:14:31 -0300

    variable and primitive types

--- src/basics/valueVarieblesTypes.scala
+++ src/basics/valueVarieblesTypes.scala
@@ -1,7 +1,39 @@
 package basics
 
-object valueVarieblesTypes {
-  def main(args:Array[String]): Unit = {
-    println("I'm ready to learn Scala!")
-  }
+object primitiveTypes extends App{
+
+  val valIntImplicit = 42
+  val valIntExplicit:Int = 24
+  println(valIntImplicit)
+  println(valIntExplicit)
+
+  //VALS ARE IMMUTABLE
+
+  //COMPILER CAN INFER TYPES
+
+  val aName: String = "Pedro"; val aName2 = "Henrique" //DENOMINE 2 OR MORE VARIABLES INLINE
+
+  val valBool: Boolean = true //LOWER CASE != PYTHON
+
+  val valChar: Char = 'a' //ONE CHARACTER
+
+  val valAnotherVal: Int = valIntImplicit //VAL WHAT VARIABLE THAT STORES ANOTHER VARIABLE
+
+  val valShortNumber: Short = 9999 ///MAX NUMBER IS 9999 (4 DIGITS)
+
+  val valLongNumber: Long = 4986469484797498L //L IN END OF NUMBER
+
+  val valFloat: Float = 2.0f //F IN END OF NUMBER
+
+  val valDouble: Double = 3.14 //DONT NEEDS F IN END OF NUMBER
+
+
+}
+
+object VariablesTypes extends App {
+
+  var valVariable: Int = 4
+  valVariable = 12
+
+  //VAR ARE MUTABLES
 }
