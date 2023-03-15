a512a176310c7bd580646048bba8af89f0e0b101
Pedro H <pedro_gonsalves@hotmail.com>
2023-03-15 00:56:35 -0300

    Revert "more hofs"
    This reverts commit f57b44a7f932d4f950fa31d2d73837913cb200b8.

--- src/Lectures/basics/Functional/HOFsCurries.scala
+++ src/Lectures/basics/Functional/HOFsCurries.scala
@@ -26,28 +26,4 @@ object HOFsCurries extends App {
   }
   println(HOfunction(expoTwo,5))
 
-  //nTimesBetter(f,n) = x => f(f(f...(x)))
-  //increment10 = nTimesBetter(plusOne,10) = x => plusOne(plusOne....(x))
-  // val y = increment10(1)
-  def nTimesBetter(f: Int => Int, n:Int): (Int => Int) =
-    if (n <= 0 ) (x: Int) => x
-    else (x: Int) => nTimesBetter(f,n-1) (f(x))
-
-  val plus10 = nTimesBetter(plusOne, 10)
-  println(plus10(1))
-
-  //curried functions
-  val superAdder: Int => (Int => Int) = (x:Int) => (y:Int) => x + y
-  val add3 = superAdder(3)
-  println(add3(10))
-  println(superAdder(3)(10))
-
-  //functions with multiple parameter lists
-  def curriedFormatter(c: String) (x: Double): String = c.format(x)
-
-  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
-  val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
-
-  println(standardFormat(Math.PI))
-  println(preciseFormat(Math.PI))
 }
